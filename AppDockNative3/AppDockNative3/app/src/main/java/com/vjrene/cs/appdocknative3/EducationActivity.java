a45c05f614e8015a96ad1e83d6693590808c61b9
Vanessa Rene <vjrene@gmail.com>
2016-07-06 22:10:33 -0400

    added edu page

--- AppDockNative3/AppDockNative3/app/src/main/java/com/vjrene/cs/appdocknative3/EducationActivity.java
+++ AppDockNative3/AppDockNative3/app/src/main/java/com/vjrene/cs/appdocknative3/EducationActivity.java
@@ -1,35 +1,112 @@
 package com.vjrene.cs.appdocknative3;
 
-import android.net.Uri;
+import android.os.AsyncTask;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
+import android.content.*;
+import android.graphics.*;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
-import android.widget.MediaController;
-import android.widget.VideoView;
+import android.widget.*;
+import java.net.*;
+import java.io.*;
 
 public class EducationActivity extends AppCompatActivity {
+
+
+
+
+    ImageView vidView;
+    String vidURL = "https://raw.githubusercontent.com/paceuniversity/cs6392016team3/master/appdockscreen.png";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_education);
 
+        ImageView homeic = (ImageView) findViewById(R.id.home);
+        Button develButton = (Button) findViewById(R.id.developerBtn);
+        Button survButton = (Button) findViewById(R.id.surveyBtn);
+        Button educaButton = (Button) findViewById(R.id.educationBtn);
 
-        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        homeic.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View view) {
+                Intent intent = new Intent(EducationActivity.this, AppPageActivity.class);
+                startActivity(intent);
+            }
+        });
 
-        VideoView videoView =(VideoView)findViewById(R.id.beyeInterview);
-        MediaController mediaController= new MediaController(this);
-        mediaController.setAnchorView(videoView);
-        Uri uri=Uri.parse("rtsp://r3---sn-p5qlsnz6.googlevideo.com/Cj0LENy73wIaNAmRAfW6tkw8GxMYDSANFC2B3nJXMOCoAUIASARg2Yb58ObsyflVigELbWc3RGRFM29LV3MM/29B17F889CCE5FB7CF256C58CC8C108555EED441.31F791D6F7EAACCDB6E0B5F0D68F15587FD364B3/yt6/1/video.3gp");
-        videoView.setMediaController(mediaController);
-        videoView.setVideoURI(uri);
-        videoView.requestFocus();
-        videoView.start();
+        develButton.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View view) {
+                Intent intent = new Intent(EducationActivity.this, DeveloperActivity.class);
+                startActivity(intent);
+            }
+        });
+        educaButton.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View view) {
+                Intent intent = new Intent(EducationActivity.this, EducationActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        survButton.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View view) {
+                Intent intent = new Intent(EducationActivity.this, SurveyActivity.class);
+                startActivity(intent);
+            }
+        });
 
 
+
+
+        vidView = (ImageView) findViewById(R.id.videoView);
+        downloadVid download = new downloadVid();
+        download.execute();
+
+
+
+    }
+
+    private class downloadVid extends AsyncTask<String, Void, Bitmap> {
+
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+        }
+
+        @Override
+        protected Bitmap doInBackground(String... params) {
+
+            try {
+                URL url = new URL(vidURL);
+                InputStream is = url.openConnection().getInputStream();
+                Bitmap finalVideo = BitmapFactory.decodeStream(is);
+
+                return finalVideo;
+
+            } catch (Exception e) {
+
+                Context context = getApplicationContext();
+                //CharSequence text = "Could not load image. Check connection.";
+                CharSequence text = "Could not load video. Check connection.";
+                int duration = Toast.LENGTH_LONG;
+
+                Toast toast = Toast.makeText(context, text, duration);
+                toast.show();
+            }
+
+            return null;
+
+        }
+
+        @Override
+        protected void onPostExecute(Bitmap bitmap) {
+
+            vidView.setImageBitmap(bitmap);
+
+        }
     }
 
 }
